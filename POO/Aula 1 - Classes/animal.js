7efdf8b1579b77b193c1f012f184bcf86639b8db
Bruno Duarte <bruno.duartec@gmail.com>
2021-06-01 18:35:30 -0300

    add POO

---
+++ POO/Aula 1 - Classes/animal.js
@@ -0,0 +1,22 @@
+class Animal {
+  constructor(nome) {
+    this.nome = nome;
+    this.comeu = 0;
+    this.andou = 0;
+  }
+
+  comer() {
+    this.comeu++;
+    console.log("Comeu");
+  }
+  andar() {
+    this.andou++;
+    console.log("Andou");
+  }
+
+  informacoes() {
+    console.log(`Animal: ${this.nome}`);
+    console.log(`Comeu: ${this.comeu} vezes`);
+    console.log(`Andou: ${this.andou} vezes`);
+  }
+}
---
+++ POO/Aula 1 - Classes/script.js
@@ -0,0 +1,13 @@
+let cachorro = new Animal("cachorro");
+
+cachorro.informacoes();
+
+cachorro.andar();
+cachorro.informacoes();
+
+let gato = new Animal("gato");
+
+gato.informacoes();
+
+gato.comer();
+gato.informacoes();
